4ce6a587159915bac98311540676ddf6091711af
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-19 21:14:08 -0400

    start grid from antd

---
+++ src/components/home/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppFeature() {
+    return (
+        <div>
+            <h1>I am feature component</h1>
+        </div>
+    )
+}
+
+export default AppFeature
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import AppHero from '../components/home/Hero'
 import AppAbout from '../components/home/About'
-
+import AppFeature from '../components/home/Feature'
 
 function AppHome() {
     return (
         <div className='main'>
             <AppHero/>
             <AppAbout/>
+            <AppFeature/>
         </div>
     )
 }
