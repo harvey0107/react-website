57348edd6e07ecbd5ab5f2d7da803996624bc5c1
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-19 21:37:08 -0400

    added more cards

--- src/components/home/Feature.js
+++ src/components/home/Feature.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Row , Col } from 'antd';
 import { Card } from 'antd';
+import image1 from '../../assets/images/modern-design.jpg';
+import image2 from '../../assets/images/clean-design.jpg';
+import image3 from '../../assets/images/great-support.jpg';
+import image4 from '../../assets/images/easy-customise.jpg';
+import image5 from '../../assets/images/unlimited-features.jpg';
+import image6 from '../../assets/images/advanced-option.jpg';
 
 
 const { Meta } = Card;
@@ -11,10 +18,49 @@ function AppFeature() {
                     <Col span={8}>
                     <Card
                         hoverable
-                        style={{ width: 240 }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        cover={<img alt="example" src={image1}/>}
                     >
-                    <Meta title="Europe Street beat" description="www.instagram.com" />
+                    <Meta title="Modern Design" />
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        cover={<img alt="example" src={image2}/>}
+                    >
+                    <Meta title="Clean and Elegant" />
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        cover={<img alt="example" src={image3}/>}
+                    >
+                    <Meta title="Great Support" />
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        cover={<img alt="example" src={image4}/>}
+                    >
+                    <Meta title="Easy to customise" />
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        cover={<img alt="example" src={image5}/>}
+                    >
+                    <Meta title="Unlimited Features" />
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        cover={<img alt="example" src={image6}/>}
+                    >
+                    <Meta title="Advanced Options" />
                     </Card>
                     </Col>
                 </Row>
