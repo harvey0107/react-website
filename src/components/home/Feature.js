76faf047393e3bf7eee62626a86dc1165c7e96d4
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-19 21:22:58 -0400

    added card from antd

--- src/components/home/Feature.js
+++ src/components/home/Feature.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Card } from 'antd';
 
+
+const { Meta } = Card;
 function AppFeature() {
     return (
-        <div>
-            <h1>I am feature component</h1>
+        <div className='block featureBlock bgGray'>
+              <div className='container-fluid'>
+              <Row gutter={[16, 16]}>
+                    <Col span={8}>
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                    <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                    </Col>
+                </Row>
+              </div>
         </div>
     )
 }
