e3323766e6425220ed7e9c3965d7743dba848cd1
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-19 20:28:20 -0400

    added contents in about page

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function AppAbout() {
     return (
-        <div>
-            <h1>I am About Page</h1>
+        <div className='block aboutBlock'>
+            <div className='container-fluid'>
+                <h2>About Us</h2>
+                <p>Tech is the first</p>
+                <div className='contentHolder'>
+                    <p>Technology is the sum of techniques, skills, methods, and processes used in the production of goods or services or in the accomplishment of objectives, such as scientific investigation.</p>
+                </div>
+            </div>
         </div>
     )
 }
