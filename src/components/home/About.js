819828a4fcbb67f8ae2551b8305b326a4adc3795
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-19 20:37:00 -0400

    added contents items

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Row, Col } from 'antd';
 
 function AppAbout() {
     return (
         <div className='block aboutBlock'>
             <div className='container-fluid'>
+                <div className='titleHolder'>
                 <h2>About Us</h2>
                 <p>Tech is the first</p>
+                </div>
                 <div className='contentHolder'>
                     <p>Technology is the sum of techniques, skills, methods, and processes used in the production of goods or services or in the accomplishment of objectives, such as scientific investigation.</p>
                 </div>
+                <Row gutter={[16, 16]}>
+                    <Col span={8}>Item</Col>
+                    <Col span={8}>Item</Col>
+                    <Col span={8}>Item</Col>
+                </Row>    
             </div>
         </div>
     )
